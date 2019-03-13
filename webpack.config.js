80683f4534bb33275330c6c0b935c5b6e42260b6
Xingmh <1173985303@qq.com>
2019-03-13 12:02:15 +0800

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: `${__dirname}/src/index.html`,
+  filename: 'index.html',
+  inject: 'body',
+});
+
+module.exports = {
+  entry: [
+    '.src/index.js',
+  ],
+  output: {
+    path: `${__dirname}/dist`,
+    filename: 'index_bundle.js',
+  },
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader',
+      query: {
+        presets: ['es2015', 'react'],
+      },
+    }],
+  },
+
+  devServer: {
+    inline: true,
+    port: 8008,
+  },
+  plugins: [HtmlWebpackPluginConfig],
+};
