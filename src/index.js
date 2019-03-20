7063a9d302f26f4d274c1e5ffd129a46d92b7497
Xminghua <1173985303@qq.com>
2019-03-20 22:05:57 +0800

    react app

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import './index.css';
-import App from './App';
-import configureStore from './redux/store';
-import { Provider } from 'react-redux';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter } from 'react-router-dom'
+import './index.css'
+import App from './App'
+import configureStore from './redux/store'
+import { Provider } from 'react-redux'
 //TODO: redux store
 
-const Root = ()=>(
-  <Provider store={store}>
-    <BrowserRouter>
-      <App/>
-    </BrowserRouter>
-  </Provider>
-);
-ReactDOM.render(<Root/>, document.getElementById('root'));
+const Root = () => {
+    return (
+        <Provider store={store}>
+            <BrowserRouter>
+            <App/>
+            </BrowserRouter>
+        </Provider>
+    )
+}
+ReactDOM.hydrate(<Root/>, document.getElementById('root'))
